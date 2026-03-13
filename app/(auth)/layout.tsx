68e2547ec90507fb4837462c7f72358451085d71
Anurodh Sahu <anurodh.sahu@innodeed.com>
2026-03-13 09:27:40 +0530

    add login page intermidiate screen

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-background ">
-      <div className="w-full max-w-[320px] ">{children}</div>
+    <div className="flex min-h-screen w-full bg-background">
+      {/* <video
+        autoPlay
+        muted
+        loop
+        className="absolute top-0 left-0  object-cover w-full h-full z-0 max-h-[400px] max-w-[400px] mx-auto"
+      >
+        <source src="/tree-animation.mp4" type="video/mp4" />
+      </video> */}
+
+      {children}
     </div>
   );
 }
--- features/auth/pages/LoginPage.tsx
+++ features/auth/pages/LoginPage.tsx
@@ -1,9 +1,61 @@
-import LoginForm from "../components/LoginForm";
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import LoginForm from "@/features/auth/components/LoginForm";
+import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
   return (
-    <div>
-      <LoginForm />
+    <div className="relative w-full h-screen px-6 py-[41px] md:py-[19px]">
+      <Image
+        src={isFormVisible ? "/tree.png" : "/tree-sappling.png"}
+        alt="tree-animation"
+        width={400}
+        height={400}
+        className="absolute top-0 left-0 object-cover w-full h-full z-0"
+      />
+
+      <div className="relative max-w-screen-2xl mx-auto z-10 flex flex-col items-center md:items-end justify-center md:justify-between w-full h-full  max-h-[800px]">
+        <header className="hidden md:block">
+          <Image src="/logo.svg" alt="logo" width={180} height={99} />
+        </header>
+        <div className="flex flex-1 items-end justify-center  md:justify-end min-w-full md:min-w-[802px] ">
+          {isFormVisible ? (
+            <div className="flex flex-1 h-full flex-col md:flex-row justify-between items-end gap-2 ">
+              <p
+                className="text-xs font-normal whitespace-nowrap"
+                style={{ letterSpacing: "0.2em", wordSpacing: "0.2em" }}
+              >
+                Partnering in your growth. Guided by wisdom.
+              </p>
+              <LoginForm />
+            </div>
+          ) : (
+            <div className="flex  gap-2 items-end align-center justify-center h-full">
+              <div className="flex flex-col md:flex-row gap-5 h-full md:h-auto justify-between items-end md:items-center md:justify-center align-center md:align-center">
+                <p
+                  className="text-xs font-normal "
+                  style={{ letterSpacing: "0.2em", wordSpacing: "0.2em" }}
+                >
+                  Every sapling of growth springs from the soil of thought.
+                </p>
+                <div className="flex gap-2 items-center align-middle justify-end">
+                  <div className="bg-primary w-[44px] h-[44px] rounded-full"></div>
+                  <Button
+                    className="border-primary text-primary h-11 rounded-full px-[47px] bg-transparent hover:bg-transparent hover:text-primary"
+                    variant={"outline"}
+                    onClick={() => setIsFormVisible(true)}
+                  >
+                    Sign In
+                  </Button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
